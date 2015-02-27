6054a3544a6f0bde8b0b2c87c8424e74f5740069
Kevin Kinnebrew <kevin.kinnebrew@gmail.com>
2015-02-27 16:11:44 -0600

    Adding click events on ledger

--- app/scripts/views/app/advanced/ledger.js
+++ app/scripts/views/app/advanced/ledger.js
@@ -1,6 +1,36 @@
 var View = require('../../../common/view');
+var $ = require('jquery');
 
 var LedgerView = View.extend({
+
+  bind: function() {
+
+    var that = this;
+
+    this._super();
+
+    this.$target.on('click', function(e) {
+      e.stopPropagation();
+      that.$target.find('.focused').removeClass('focused');
+    });
+
+    this.$target.on('click', '.text-field', function(e) {
+      e.stopPropagation();
+      console.log('123');
+      var cell = $(this).closest('.table-cell');
+      that.$target.find('.focused').removeClass('focused');
+      cell.addClass('focused');
+    });
+
+  },
+
+  unbind: function() {
+
+    this._super();
+
+
+
+  }
 
 });
 
