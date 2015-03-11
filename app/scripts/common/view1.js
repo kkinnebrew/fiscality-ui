dffb4a8328c35a881553049e435a1cdcda6f2820
Kevin Kinnebrew <kevin.kinnebrew@gmail.com>
2015-03-11 16:25:53 -0500

    Fix for event bubbling outside their scope, HACKY

--- app/scripts/common/view1.js
+++ app/scripts/common/view1.js
@@ -2,16 +2,31 @@ var $ = require('jquery');
 
 function View(template) {
 
+  /**
+   * stores a copy of the template provided to the view
+   * @param {Function|string}
+   */
   this.template = template;
 
+  /**
+   * holds a reference to where the view is rendered into the DOM
+   * @type {jQuery}
+   */
   this.$el = null;
 
+  /**
+   * whether or not the view has been rendered
+   * @type {boolean}
+   */
   this.rendered = false;
-
-  console.log('Initializing base view');
 
 }
 
+/**
+ * renders the view directly into a given element
+ * @method render
+ * @param $el
+ */
 View.prototype.render = function($el) {
 
   // check if rendered
@@ -20,7 +35,11 @@ View.prototype.render = function($el) {
     return console.warn('View is already rendered');
   }
 
+  // store reference to DOM
+
   this.$el = $el;
+
+  // render the template
 
   var html = typeof this.template === 'function' ? this.template() : this.template;
 
@@ -28,24 +47,43 @@ View.prototype.render = function($el) {
 
   this.rendered = true;
 
-  this.bind();
+  // bind event handlers
 
+  this.$el.find('[ui-view]').on('click', function(e) {
+    e.stopPropagation();
+  });
+
+  this.bind();
 
 };
 
+/**
+ * method called after rendering when events should be bound
+ * @method bind
+ */
 View.prototype.bind = function() {
 
 };
 
+/**
+ * method called prior to destroying all DOM references, when events should be unbound
+ * @method unbind
+ */
 View.prototype.unbind = function() {
 
 };
 
+/**
+ * destroys the currently rendered view and returns it to its unrendered state
+ * @method destroy
+ */
 View.prototype.destroy = function() {
 
   if (!this.rendered) {
     return console.warn('Unrendered view cannot be destroyed');
   }
+
+  this.$el.find('[ui-view]').off('click');
 
   this.unbind();
 
@@ -55,6 +93,12 @@ View.prototype.destroy = function() {
 
 };
 
+/**
+ * returns a view reference to a subview
+ * @method getSubview
+ * @param name
+ * @returns {*}
+ */
 View.prototype.getSubview = function(name) {
 
   var subview = null;
