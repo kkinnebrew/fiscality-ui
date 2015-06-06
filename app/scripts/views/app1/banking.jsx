78ce8f5e5e2135d5d1399f7f1e9e076d785ed4af
Kevin Kinnebrew <kevin.kinnebrew@gmail.com>
2015-06-06 13:55:38 -0500

    Binding new routes

--- app/scripts/views/app1/banking.jsx
+++ app/scripts/views/app1/banking.jsx
@@ -1,4 +1,7 @@
 var React = require('react');
+var BankingToolbar = require('./banking/toolbar.jsx');
+var TransactionTable = require('./banking/transaction-table.jsx');
+var TransactionDetail = require('./banking/transaction-detail.jsx');
 
 var BankingView = React.createClass({
 
--- app/scripts/views/app1/investments.jsx
+++ app/scripts/views/app1/investments.jsx
@@ -1,4 +1,7 @@
 var React = require('react');
+var InvestmentsToolbar = require('./investments/toolbar.jsx');
+var InvestmentsChart = require('./investments/chart.jsx');
+var HoldingsTable = require('./investments/holdings-table.jsx');
 
 var InvestmentsView = React.createClass({
 
