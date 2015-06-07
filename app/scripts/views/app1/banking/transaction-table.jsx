6755b7fac8c78b0053d02e79b107b6c5092c1f39
Kevin Kinnebrew <kevin.kinnebrew@gmail.com>
2015-06-07 14:49:00 -0500

    Minor css adjustments

--- app/scripts/views/app1/banking/transaction-table.jsx
+++ app/scripts/views/app1/banking/transaction-table.jsx
@@ -8,10 +8,10 @@ var TransactionTable = React.createClass({
         <div className="table-head">
           <div className="header-row">
             <div className="header-column selected ascending">Date</div>
-            <div className="header-column">Type</div>
+            <div className="header-column md">Type</div>
             <div className="header-column xl">Description</div>
-            <div className="header-column">Amount</div>
-            <div className="header-column">Balance</div>
+            <div className="header-column right last">Balance</div>
+            <div className="header-column right">Amount</div>
           </div>
         </div>
         <div className="table-body">
@@ -19,7 +19,7 @@ var TransactionTable = React.createClass({
             <div className="column">
               <div className="label">8/19/2014</div>
             </div>
-            <div className="column">
+            <div className="column md">
               <div className="label">Paycheck Deposit</div>
             </div>
             <div className="column xl">
@@ -29,11 +29,11 @@ var TransactionTable = React.createClass({
                 <div className="tag">Taxable</div>
               </div>
             </div>
-            <div className="column">
-              <div className="label">1,503.21</div>
-            </div>
-            <div className="column">
+            <div className="column right last">
               <div className="label">$3,912.61</div>
+            </div>
+            <div className="column right">
+              <div className="label">1,503.21</div>
             </div>
           </div>
         </div>
